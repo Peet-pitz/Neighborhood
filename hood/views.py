4e21bbb444399a84ca89bebd53702dabb2946296
Peter Njoroge <njoroge.pitz@gmail.com>
2018-11-26 06:23:25 +0300

    create edit profile view function

--- hood/views.py
+++ hood/views.py
@@ -113,18 +113,18 @@ def profile(request):
     # except ObjectDoesNotExist:
     #     return redirect('create-profile')
 
-# def edit_profile(request):
-#     current_user = request.user
-#     if request.method == 'POST':
-#         form = ProfileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             profile = form.save(commit = False)
-#             profile.user = current_user
-#             profile.save()
-#         return redirect('Profile')
-#     else:
-#         form = ProfileForm()
-#     return render(request,'edit_profile.html',{'form':form})
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.user = current_user
+            profile.save()
+        return redirect('Profile')
+    else:
+        form = ProfileForm()
+    return render(request,'edit_profile.html',{'form':form})
 
 # def search_results(request):
 
